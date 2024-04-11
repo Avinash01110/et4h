d10e2b21e47866948fbeca5c109004d1e8e6c1e0
Avinash01110 <gavinash321098@gmail.com>
2024-04-11 20:40:09 +0530

    v 1.2.3

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,15 +5,28 @@ import lp_image1 from "../Photos/lp_image1.png";
 import lp_image2 from "../Photos/lp_image2.png";
 import lp_image3 from "../Photos/lp_image3.png";
 import leadership from "../Photos/leadership1.mp4";
+import liverimage from "../Photos/liverimage.png";
+import kidneyimage from "../Photos/kidneyimage.png";
+import breastimage from "../Photos/breastimage.png";
+import ECGimage from "../Photos/ECGimage.png";
+import EEGimage from "../Photos/EEGimage.png";
 
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "../style/Home.css";
-import { Autoplay, Mousewheel, Navigation, Pagination} from "swiper/modules";
+import { Autoplay, Mousewheel, Navigation, Pagination } from "swiper/modules";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(useGSAP, ScrollTrigger);
+
+
 
 export default function Home() {
+
+
   const latestPost = [
     {
       title: "The Future of Healthcare : AI Innovations",
@@ -38,6 +51,56 @@ export default function Home() {
     },
   ];
 
+  const Projects = [
+    {
+      index: "01",
+      title: "Liver Tumor Segmentation",
+      description: "description",
+      link: "/"
+    },
+    {
+      index: "02",
+      title: "Kidney Tumor Segmentation",
+      description: "description",
+      link: "/"
+    },
+    {
+      index: "03",
+      title: "Breast Tumor Segmentation",
+      description: "description",
+      link: "/"
+    },
+    {
+      index: "04",
+      title: "Electroencephalogram (EEG)",
+      description: "description",
+      link: "/"
+    },
+    {
+      index: "05",
+      title: "Electrocardiogram (ECG)",
+      description: "description",
+      link: "/"
+    },
+  ];
+
+  const Projectsimage = [
+    {
+      image: liverimage,
+    },
+    {
+      image: kidneyimage,
+    },
+    {
+      image: breastimage,
+    },
+    {
+      image: EEGimage,
+    },
+    {
+      image: ECGimage,
+    },
+  ];
 
   const swiperRef = useRef(null);
 
@@ -53,8 +116,40 @@ export default function Home() {
     }
   };
 
+  const animationRef = useRef(null);
+
+  useGSAP(() => {
+    const rightPhotoItems = gsap.utils.toArray(".right-photo-item");
+
+    rightPhotoItems.forEach(function (item, index) {
+      item.style.zIndex = rightPhotoItems.length - index;
+    });
+
+    gsap.set(".right-photo-item", {
+      clipPath: "inset(0px 0px 0px 0px)",
+    });
+
+    animationRef.current = gsap.to(".right-photo-item:not(:last-child)", {
+      clipPath: "inset(0px 0px 100% 0px)",
+      stagger: 0.5,
+      ease: "none",
+    });
+
+    ScrollTrigger.create({
+      trigger: ".projects",
+      scroller: "body",
+      start: "top top",
+      end: "bottom bottom",
+      animation: animationRef.current,
+      scrub: 1,
+    });
+  });
+
+
+
   return (
     <>
+      {/* Landing page */}
       <div className="h-[100vh] w-full bg-lightblue flex items-end">
         <div className="h-[29rem] w-full flex">
           <div className="left w-1/2 h-full px-20">
@@ -105,6 +200,7 @@ export default function Home() {
         </div>
       </div>
 
+      {/* Our Members - Logos */}
       <div className="border-b-2 border-darkblue h-auto w-full bg-lightblue px-28 py-28 flex flex-col items-center gap-y-12">
         <h2 className="text-5xl text-blue font-bold uppercase [text-shadow:3px_6px_10px_var(--tw-shadow-color)] shadow-grey">
           Our Members
@@ -144,10 +240,9 @@ export default function Home() {
         </div>
       </div>
 
+      {/* Latest Post */}
       <div className="border-b-2 border-darkblue h-auto w-full bg-purewhite flex justify-center items-center py-20">
-        
         <div className="crousel h-auto w-3/4 bg-lightblue rounded-lg p-10 flex flex-col gap-y-10 shadow-lg shadow-grey">
-
           <h2 className="text-4xl text-grey font-bold uppercase [text-shadow:3px_6px_10px_var(--tw-shadow-color)] shadow-grey">
             Latest Posts
           </h2>
@@ -155,8 +250,8 @@ export default function Home() {
           <Swiper
             ref={swiperRef}
             navigation={{
-            prevEl: '.custom-prev',
-            nextEl: '.custom-next',
+              prevEl: ".custom-prev",
+              nextEl: ".custom-next",
             }}
             autoplay={{
               delay: 2000,
@@ -166,13 +261,13 @@ export default function Home() {
             loop={true}
             mousewheel={true}
             slidesPerView={1}
-            pagination={{clickable: true}}
-            modules={[Mousewheel,Pagination,Navigation, Autoplay]}
+            pagination={{ clickable: true }}
+            modules={[Mousewheel, Pagination, Navigation, Autoplay]}
             className="mySwiper"
           >
             {latestPost.map((post, index) => (
               <SwiperSlide key={index}>
-                <div  className="h-[20rem] w-full bg-white p-10 flex flex-row rounded-lg">
+                <div className="h-[20rem] w-full bg-white p-10 flex flex-row rounded-lg">
                   <div className="h-full w-7/12 p-4 flex flex-col justify-between">
                     <div className="flex flex-col gap-y-5 pr-16">
                       <h3 className="text-2xl text-blue font-bold">
@@ -203,73 +298,140 @@ export default function Home() {
           </Swiper>
 
           <div className="flex justify-end gap-x-4 pr-10">
-            <div onClick={goPrev} className="mt-1 flex h-10 w-10 flex-none items-center justify-center rounded-full bg-lightgrey group hover:bg-purewhite cursor-pointer transition ease-in-out duration-300">
+            <div
+              onClick={goPrev}
+              className="mt-1 flex h-10 w-10 flex-none items-center justify-center rounded-full bg-lightgrey group hover:bg-purewhite cursor-pointer transition ease-in-out duration-300"
+            >
               <IoIosArrowBack
                 className="h-5 w-5 text-grey group-hover:text-darkblue transition ease-in-out duration-300"
                 aria-hidden="true"
               />
             </div>
-            <div onClick={goNext} className="mt-1 flex h-10 w-10 flex-none items-center justify-center rounded-full bg-lightgrey group hover:bg-purewhite cursor-pointer transition ease-in-out duration-300">
+            <div
+              onClick={goNext}
+              className="mt-1 flex h-10 w-10 flex-none items-center justify-center rounded-full bg-lightgrey group hover:bg-purewhite cursor-pointer transition ease-in-out duration-300"
+            >
               <IoIosArrowForward
                 className="h-5 w-5 text-grey group-hover:text-darkblue transition ease-in-out duration-300"
                 aria-hidden="true"
               />
             </div>
           </div>
-
         </div>
       </div>
 
-      <div className="border-b-2 border-grey h-[30rem] w-full bg-lightblue flex justify-start items-center relative group overflow-hidden">
-        <div className="about bg-[url('https://img.freepik.com/free-photo/medium-shot-scientists-lab_23-2148970019.jpg?t=st=1712730077~exp=1712733677~hmac=bedf57e8cb70e08b3778d1ee268ea54a591de2697719a6ed13c7327098a90c1c&w=740')] h-full w-full bg-cover bg-no-repeat gradient-mask-l-[rgba(0,0,0,1.0)_10%,rgba(0,0,0,1.0)_30px,rgba(0,0,0,0.3)_40%] group-hover:scale-110 transition ease-in-out duration-1000">
+      {/* Active Project */}
+      <div className="border-b-2 border-grey bg-lightblue h-auto w-full flex flex-col items-center py-20 gap-y-1">
+        <h2 className="text-5xl text-blue font-bold uppercase [text-shadow:2px_4px_5px_var(--tw-shadow-color)] shadow-grey">
+          Active Projects
+        </h2>
+
+        <div className="projects w-full h-auto px-16 flex flex-row">
+          
+          <div className="left w-1/2 h-auto flex items-center justify-center">
+              <div className="projects-text">
+              {Projects.map((project, index) => (
+                <div key={index} className="h-[100vh] project-info flex flex-col justify-center gap-y-10">
+                  <div className="flex flex-col gap-y-1">
+                    <span className="text-2xl text-grey font-semibold">
+                      {project.index}
+                    </span>
+                    <h3 className="text-4xl text-grey font-bold">
+                      {project.title}
+                    </h3>
+                    <span className="text-base text-grey">
+                      {project.description}
+                    </span>
+                  </div>
+                  <Link to={project.link}>
+                    <button className="button bg-blue text-[#FFFFFF] py-2 px-3 rounded-lg text-sm font-medium active:bg-blue hover:bg-darkblue hover:shadow-md hover:shadow-lightgrey transition ease-in-out duration-300">
+                      View More
+                    </button>
+                  </Link>
+                </div>
+              ))}
+              </div>
+          </div>
+
+
+          <div className="right w-1/2 h-auto">
+            <div className="right-b1 flex flex-col justify-center sticky top-0 w-full h-[100vh]">
+              <div
+                useRef={animationRef}
+                className="right-photo h-96 w-[40rem] relative"
+              >
+                {Projectsimage.map((project, index) => (
+                <div key={index} className="right-photo-item w-full h-full py-5 px-10 absolute">
+                  <img
+                    className="h-full w-full object-cover rounded-lg block border-2 border-grey"
+                    src={project.image}
+                    alt="error"
+                  />
+                </div>
+                 ))}
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
+
+      {/* About us */}
+      <div className="border-b-2 border-grey h-[30rem] w-full bg-lightblue flex justify-start items-center relative group overflow-hidden">
+        <div className="about bg-[url('https://img.freepik.com/free-photo/medium-shot-scientists-lab_23-2148970019.jpg?t=st=1712730077~exp=1712733677~hmac=bedf57e8cb70e08b3778d1ee268ea54a591de2697719a6ed13c7327098a90c1c&w=740')] h-full w-full bg-cover bg-no-repeat gradient-mask-l-[rgba(0,0,0,1.0)_10%,rgba(0,0,0,1.0)_30px,rgba(0,0,0,0.3)_40%] group-hover:scale-110 transition ease-in-out duration-1000"></div>
         <div className="w-[35rem] h-full flex flex-col justify-center items-center gap-y-10 absolute p-8 ml-10">
           <div className="flex flex-col gap-y-7">
-          <h3 className="text-blue text-5xl font-bold">About Us</h3>
-          <span className="text-grey text-sm font-medium text-justify opacity-80">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
-          eget lectus auctor, iaculis leo ut, malesuada urna. Phasellus
-          tincidunt sit amet orci vitae laoreet. Quisque tellus lacus,
-          bibendum nec diam a, laoreet commodo mauris. Nulla volutpat
-          congue dolor, sit amet vulputate nisl hendrerit pellentesque.
-          Cras sollicitudin est at lectus fringilla ornare. Aeneanngue
-          sollicitudin, dui nisi varius elit, sed ullamcorper nulla ligula sed
-          leo.
-          </span>
+            <h3 className="text-blue text-5xl font-bold">About Us</h3>
+            <span className="text-grey text-sm font-medium text-justify opacity-80">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+              eget lectus auctor, iaculis leo ut, malesuada urna. Phasellus
+              tincidunt sit amet orci vitae laoreet. Quisque tellus lacus,
+              bibendum nec diam a, laoreet commodo mauris. Nulla volutpat congue
+              dolor, sit amet vulputate nisl hendrerit pellentesque. Cras
+              sollicitudin est at lectus fringilla ornare. Aeneanngue
+              sollicitudin, dui nisi varius elit, sed ullamcorper nulla ligula
+              sed leo.
+            </span>
           </div>
           <div className="flex justify-end w-full">
-          <button className="button bg-blue text-[#FFFFFF] py-3 px-5 rounded-lg text-sm font-medium active:bg-blue hover:bg-darkblue hover:shadow-md hover:shadow-lightgrey transition ease-in-out duration-300">
-                Discover More 
-          </button>
+            <button className="button bg-blue text-[#FFFFFF] py-3 px-5 rounded-lg text-sm font-medium active:bg-blue hover:bg-darkblue hover:shadow-md hover:shadow-lightgrey transition ease-in-out duration-300">
+              Discover More
+            </button>
           </div>
         </div>
       </div>
 
+      {/* Our Leadership */}
       <div className="h-auto w-full flex flex-row items-center">
         <div className="h-auto w-7/12">
-          <video src={leadership} className="h-full w-full object-cover" autoPlay loop muted/>
+          <video
+            src={leadership}
+            className="h-full w-full object-cover p-2"
+            autoPlay
+            loop
+            muted
+          />
         </div>
         <div className="h-full w-5/12">
-        <div className="w-[35rem] h-full flex flex-col justify-center items-center gap-y-10 px-8">
-          <div className="flex flex-col gap-y-7">
-          <h3 className="text-grey text-5xl font-bold">Our Leadership</h3>
-          <span className="text-grey text-sm font-medium text-justify opacity-80">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
-          eget lectus auctor, iaculis leo ut, malesuada urna. Phasellus
-          tincidunt sit amet orci vitae laoreet. Quisque tellus lacus,
-          bibendum nec diam a, laoreet commodo mauris. Nulla volutpat
-          congue dolor, sit amet vulputate nisl hendrerit pellentesque.
-          Cras sollicitudin est at lectus fringilla ornare. Aeneanngue
-          sollicitudin, dui nisi varius elit, sed ullamcorper nulla ligula sed
-          leo.
-          </span>
+          <div className="w-[35rem] h-full flex flex-col justify-center items-center gap-y-10 px-8">
+            <div className="flex flex-col gap-y-7">
+              <h3 className="text-grey text-5xl font-bold">Our Leadership</h3>
+              <span className="text-grey text-sm font-medium text-justify opacity-80">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+                eget lectus auctor, iaculis leo ut, malesuada urna. Phasellus
+                tincidunt sit amet orci vitae laoreet. Quisque tellus lacus,
+                bibendum nec diam a, laoreet commodo mauris. Nulla volutpat
+                congue dolor, sit amet vulputate nisl hendrerit pellentesque.
+                Cras sollicitudin est at lectus fringilla ornare. Aeneanngue
+                sollicitudin, dui nisi varius elit, sed ullamcorper nulla ligula
+                sed leo.
+              </span>
+            </div>
+            <div className="flex justify-start w-full">
+              <button className="button bg-blue text-[#FFFFFF] py-3 px-5 rounded-lg text-sm font-medium active:bg-blue hover:bg-darkblue hover:shadow-md hover:shadow-lightgrey transition ease-in-out duration-300">
+                Meet Team
+              </button>
+            </div>
           </div>
-          <div className="flex justify-start w-full">
-          <button className="button bg-blue text-[#FFFFFF] py-3 px-5 rounded-lg text-sm font-medium active:bg-blue hover:bg-darkblue hover:shadow-md hover:shadow-lightgrey transition ease-in-out duration-300">
-                Meet Team 
-          </button>
-          </div>
-        </div>
         </div>
       </div>
     </>
