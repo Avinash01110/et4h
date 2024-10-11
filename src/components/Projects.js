5a2eab4a1fcdff30649be65f97dc62cf24dffd85
Avinash01110 <gavinash321098@gmail.com>
2024-10-11 12:50:24 +0530

    Refactor Projects component to improve image loading and add view more button

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import { Helmet } from "react-helmet-async";
+import { fetchProject } from "../services/fetchData";
 import liverimage from "../Photos/Projects/liverimage.png";
 import kidneyimage from "../Photos/Projects/kidneyimage.png";
 import breastimage from "../Photos/Projects/breastimage.png";
@@ -11,8 +12,6 @@ import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import Loader from "./Loader";
 gsap.registerPlugin(useGSAP, ScrollTrigger);
-
-import { fetchProject } from "../services/fetchData";
 
 export default function Projects() {
   const [projects, setProjects] = useState(null);
@@ -107,7 +106,7 @@ export default function Projects() {
   const getProjects = async () => {
     try {
       const data = await fetchProject();
-      setProjects(data.posts)
+      setProjects(data.posts);
     } catch (err) {
       setError("Failed to load teams");
     } finally {
@@ -136,38 +135,39 @@ export default function Projects() {
           <div className="projects w-full h-auto px-4 py-36 sm:py-36 lg:py-0 lg:px-6 xl:px-10 2xl:px-16 flex flex-row">
             <div className="left w-full sm:w-full lg:w-1/2 h-auto flex items-center justify-center">
               <div className="projects-text flex flex-col gap-y-36">
-                {projects && projects.map((project, index) => (
-                  <div
-                    key={project._id}
-                    className="h-auto sm:h-auto lg:h-[100vh] project-info flex flex-col justify-center items-center sm:items-center lg:items-start gap-y-10"
-                  >
-                    <div className="flex sm:flex lg:hidden h-96 w-auto">
-                      <div className="w-96 xs:w-[30rem] sm:w-[38rem] h-96 sm:h-96  px-4 sm:px-4">
-                        <img
-                          className="h-full w-full object-cover rounded-lg block border-2 border-grey"
-                          src={project.image}
-                          alt="error"
-                        />
+                {projects &&
+                  projects.map((project, index) => (
+                    <div
+                      key={project._id}
+                      className="h-auto sm:h-auto lg:h-[100vh] project-info flex flex-col justify-center items-center sm:items-center lg:items-start gap-y-10"
+                    >
+                      <div className="flex sm:flex lg:hidden h-96 w-auto">
+                        <div className="w-96 xs:w-[30rem] sm:w-[38rem] h-96 sm:h-96  px-4 sm:px-4">
+                          <img
+                            className="h-full w-full object-cover rounded-lg block border-2 border-grey"
+                            src={project.image}
+                            alt="error"
+                          />
+                        </div>
                       </div>
-                    </div>
-                    <div className="flex flex-wrap flex-col w-full sm:w-full lg:w-auto px-6 sm:px-6 lg:px-0 gap-y-1">
-                      {/* <span className="text-2xl text-grey font-semibold font-sans">
+                      <div className="flex flex-wrap flex-col w-full sm:w-full lg:w-auto px-6 sm:px-6 lg:px-0 gap-y-1">
+                        {/* <span className="text-2xl text-grey font-semibold font-sans">
                         {project.index}
                       </span> */}
-                      <h3 className="text-2xl sm:text-4xl text-grey font-bold font-sans">
-                        {project.title}
-                      </h3>
-                      <span className="text-base text-grey font-sans">
-                        {project.shortDesc}
-                      </span>
+                        <h3 className="text-2xl sm:text-4xl text-grey font-bold font-sans">
+                          {project.title}
+                        </h3>
+                        <span className="text-base text-grey font-sans">
+                          {project.shortDesc}
+                        </span>
+                      </div>
+                      <Link to={project.link + "/" + project.title}>
+                        <button className="button bg-blue text-[#FFFFFF] py-2 px-3 rounded-lg text-sm font-medium font-sans active:bg-blue hover:bg-darkblue hover:shadow-md hover:shadow-lightgrey transition ease-in-out duration-300">
+                          View More
+                        </button>
+                      </Link>
                     </div>
-                    <Link to={project.link + "/" + project.title}>
-                      <button className="button bg-blue text-[#FFFFFF] py-2 px-3 rounded-lg text-sm font-medium font-sans active:bg-blue hover:bg-darkblue hover:shadow-md hover:shadow-lightgrey transition ease-in-out duration-300">
-                        View More
-                      </button>
-                    </Link>
-                  </div>
-                ))}
+                  ))}
               </div>
             </div>
 
@@ -177,19 +177,20 @@ export default function Projects() {
                   useRef={animationRef}
                   className="right-photo h-[28rem] xl:w-auto 2xl:w-auto relative"
                 >
-                  {projects && projects.map((project, index) => (
-                    <div
-                      key={index}
-                      className="right-photo-item w-full h-full py-5 px-10 absolute"
-                    >
-                      <img
-                        className="h-full w-full object-cover rounded-lg block border-2 border-grey"
-                        src={project.image}
-                        alt="Project Image"
-                        loading="lazy"
-                      />
-                    </div>
-                  ))}
+                  {projects &&
+                    projects.map((project, index) => (
+                      <div
+                        key={index}
+                        className="right-photo-item w-full h-full py-5 px-10 absolute"
+                      >
+                        <img
+                          className="h-full w-full object-cover rounded-lg block border-2 border-grey"
+                          src={project.image}
+                          alt="Project Image"
+                          loading="lazy"
+                        />
+                      </div>
+                    ))}
                 </div>
               </div>
             </div>
