4c1d51e2345304029bb741a66d82443a48dd9169
Avinash01110 <gavinash321098@gmail.com>
2024-10-23 01:36:37 +0530

    Refactor index.js to update Redux store configuration and remove unused imports

--- src/index.js
+++ src/index.js
@@ -4,13 +4,10 @@ import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { configureStore } from "@reduxjs/toolkit";
-import rootReducer from "./reducers"; // Ensure this path is correct
-import { Helmet, HelmetProvider } from 'react-helmet-async';
+import rootReducer from "./reducers";
 
-import { createStore, applyMiddleware } from 'redux';
-import {thunk} from 'redux-thunk';
-// import rootReducer from './reducers';
+import { createStore, applyMiddleware } from "redux";
+import { thunk } from "redux-thunk";
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
@@ -19,9 +16,7 @@ root.render(
   <>
     <Provider store={store}>
       <BrowserRouter>
-      <HelmetProvider>
         <App />
-      </HelmetProvider>
       </BrowserRouter>
     </Provider>
   </>
